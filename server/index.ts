ff8bad48dabf0c1a63863e3c4365d89e0c6f469c
the570j <crazybomb47@outlook.com>
2025-04-25 16:13:04 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -47,17 +47,14 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
+  
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // Use the port from environment variables (for Glitch compatibility) or default to 5000
-  // this serves both the API and the client
+  
   const port = process.env.PORT || 5000;
   server.listen({
     port: Number(port),
